f6a351b55140b6947b6ae3cbbf29c6c71aa227ac
cassle black <125638729+Cassleblack@users.noreply.github.com>
2024-03-29 19:30:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,33 @@ function getComputerChoice() {
 return Math.floor(Math.random() * words.length);
 }
 
-let computerChoice = words[getComputerChoice()];
+let playerInput = prompt("Play: " + "Rock, Paper, Scissors");
+playerInput = playerInput.trim();
+function capitalize(input) {
+return input.charAt(0).toUpperCase() + input.slice(1).toLowerCase();
+}
 
-console.log(computerChoice);
+let playerSelection = capitalize(playerInput);
+
+function gamePlay (player, Computer) {
+if (player === "Rock" && computer === "Scissors") {
+return "You Win!!! Rock beats Scissors.";
+} else if (player === "Paper" && computer === "Rock") {
+return "You Win!!! Paper beats Rock.";
+} else if (player === "Scissors" && computer === "Paper") {
+return "You Win!!! Scissors beats Paper.";
+} else if (player === "Rock" && computer === "Paper") {
+return "You Lose!!! Paper beats Rock.";
+} else if (player === "Scissors" && computer === "Rock") {
+return "You Lose!!! Rock beats Paper.";
+} else if (player === "Paper" && computer === "Scissors") {
+return "You Lose!!! Scissors beats Paper.";
+} else if (player === computer && computer === player) {
+return "T's a Tie!!!";
+} else {
+return "OOPs Wrong Input!";
+}
+}
+
+let computerSelection = getComputerChoice;
+let result = gamePlay(playerSelection, computerSelection);
