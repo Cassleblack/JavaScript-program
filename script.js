eaa2616ff5e8b0e27dc789de14f12dac2a9745bb
cassle black <125638729+Cassleblack@users.noreply.github.com>
2024-03-29 20:44:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,49 @@
 const words = ["Rock" , "Paper" , "Scissors" ];
 
 function getComputerChoice() {
-return Math.floor(Math.random() * words.length);
+    return Math.floor(Math.random() * words.length);
+    }
+
+let playerWins = 0;
+let computerWins = 0;
+
+function playGame() {
+    let playerInput = prompt("Play: " + "Rock, Paper, Scissors");
+    
+    playerInput = playerInput.trim();
+    
+    function capitalize(input) {
+        return input.charAt(0).toUpperCase() + input.slice(1).toLowerCase();
+        }
+        
+        let playerSelection = capitalize(playerInput);
+        let computerSelection = words[getComputerChoice()];
+        
+        function gamePlay (player, computer) {
+            if (player === "Rock" && computer === "Scissors" || player === "Paper" && computer === "Rock" || player === "Scissors" && computer === "Paper") {
+                playerWins++;
+                return `You Win!!! ${player} beats ${computer}.`;
+            } else if (player === computer) {
+                return `T's a Tie!!!`;
+            } else {
+                computerWins++;
+                return `You Lose!!! ${computer} beats ${player}.`;
+            }
+        }
+        
+        let result = gamePlay(playerSelection, computerSelection);
 }
 
-let playerInput = prompt("Play: " + "Rock, Paper, Scissors");
-playerInput = playerInput.trim();
-function capitalize(input) {
-return input.charAt(0).toUpperCase() + input.slice(1).toLowerCase();
-}
+playgame();
+playgame();
+playgame();
+playgame();
+playgame();
 
-let playerSelection = capitalize(playerInput);
-
-function gamePlay (player, computer) {
-if (player === "Rock" && computer === "Scissors") {
-return "You Win!!! Rock beats Scissors.";
-} else if (player === "Paper" && computer === "Rock") {
-return "You Win!!! Paper beats Rock.";
-} else if (player === "Scissors" && computer === "Paper") {
-return "You Win!!! Scissors beats Paper.";
-} else if (player === "Rock" && computer === "Paper") {
-return "You Lose!!! Paper beats Rock.";
-} else if (player === "Scissors" && computer === "Rock") {
-return "You Lose!!! Rock beats Scissors.";
-} else if (player === "Paper" && computer === "Scissors") {
-return "You Lose!!! Scissors beats Paper.";
-} else if (player === computer) {
-return "T's a Tie!!!";
+if (playWins > computerWins) {
+    console.log("Final Winner: You!");
+} else if (playWins < computerWins) {
+    console.log("Final Winner: Computer!");
 } else {
-return "OOPs Wrong Input!";
+    console.log("T's a Tie!");
 }
-}
-
-let computerSelection = words[getComputerChoice()];
-let result = gamePlay(playerSelection, computerSelection);
