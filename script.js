331ef192b420dc56fcce69cc38061a55b49a5d8c
olowe christian <olowechristian@gmail.com>
2024-10-03 16:32:34 +0100

    JavaScript-program new game button added

--- script.js
+++ script.js
@@ -1,53 +1,129 @@
 const words = ["Rock" , "Paper" , "Scissors" ];
 
-function getComputerChoice() {
-    return Math.floor(Math.random() * words.length);
-    }
-
 let playerWins = 0;
 let computerWins = 0;
+let roundsPlayed = 0;
 
-function playGame() {
-    let playerInput = prompt("Play: " + "Rock, Paper, Scissors");
-    
-    playerInput = playerInput.trim();
-    
-    function capitalize(input) {
-        return input.charAt(0).toUpperCase() + input.slice(1).toLowerCase();
-        }
-        
-        let playerSelection = capitalize(playerInput);
-        let computerSelection = words[getComputerChoice()];
-        
-        function gamePlay (player, computer) {
-            if (player === "Rock" && computer === "Scissors" || player === "Paper" && computer === "Rock" || player === "Scissors" && computer === "Paper") {
-                playerWins++;
-                return `You Win!!! ${player} beats ${computer}.
-                Your score: ${playerWins} Computer's score: ${computerWins}.`;
-            } else if (player === computer) {
-                return `T's a Tie!!!
-                Your score: ${playerWins} Computer's score: ${computerWins}.`;
-            } else {
-                computerWins++;
-                return `You Lose!!! ${computer} beats ${player}.
-                Your score: ${playerWins} Computer's score: ${computerWins}.`;
-            }
-        }
-        
-        let result = gamePlay(playerSelection, computerSelection);
-        console.log(result);
+function getComputerChoice() {
+    return Math.floor(Math.random() * words.length);
 }
 
-playGame();
-playGame();
-playGame();
-playGame();
-playGame();
+function playGame () {
+    
+    let btnId = "";
+    let playerChoice = "";
+    let computerChoice = "";
+    let result = "";
+    
+    const btnRock = document.getElementById("Rock");
+    btnRock.addEventListener ("click", (event) => {
+        if (roundsPlayed < 5) {
+            btnId = event.target.id;
+            playerChoice = playerSelection(btnId);
+            computerChoice = words[getComputerChoice()];
+            result = gamePlay(playerChoice, computerChoice);
+            displayResult(result);
+            roundsPlayed++;
+            if (roundsPlayed === 5) displayWinner();
+        }
+    });
+    
+    const btnPaper = document.getElementById("Paper");
+    btnPaper.addEventListener ("click", (event) => {
+        if (roundsPlayed < 5) {
+            btnId = event.target.id;
+            playerChoice = playerSelection(btnId);
+            computerChoice = words[getComputerChoice()];
+            result = gamePlay(playerChoice, computerChoice);
+            displayResult(result);
+            roundsPlayed++;
+            if (roundsPlayed === 5) displayWinner();
+        }
+    });
+    
+    const btnScissors = document.getElementById("Scissors");
+    btnScissors.addEventListener ("click", (event) => {
+        if (roundsPlayed < 5) {
+            btnId = event.target.id;
+            playerChoice = playerSelection(btnId);
+            computerChoice = words[getComputerChoice()];
+            result = gamePlay(playerChoice, computerChoice);
+            displayResult(result);
+            roundsPlayed++;
+            if (roundsPlayed === 5) displayWinner();
+        }
+    });
 
-if (playerWins > computerWins) {
-    console.log("Final Winner: You!");
-} else if (playerWins < computerWins) {
-    console.log("Final Winner: Computer!");
-} else {
-    console.log("T's a Tie!");
+    const btnNewGame = document.getElementById("newGame");
+    btnNewGame.addEventListener("click", () => {
+        newGame();
+    });
+
+
+
+    function playerSelection (buttonId) {
+        if (buttonId === "Rock") {
+            return "Rock";
+        }
+
+        else if (buttonId === "Paper") {
+            return "Paper";
+        }
+
+        else if (buttonId === "Scissors") {
+            return "Scissors";
+        }
+
+        else {
+            return "Error: Invalid Selection";
+        }
+    }
+    
+    function gamePlay (player, computer) {
+        if (player === "Rock" && computer === "Scissors" || player === "Paper" && computer === "Rock" || player === "Scissors" && computer === "Paper") {
+            playerWins++;
+            return `You Win!!! ${player} beats ${computer}.<br>
+            Your score: ${playerWins}<br>Computer's score: ${computerWins}`;
+        } else if (player === computer) {
+            return `It's a Tie!!!<br>
+            Your score: ${playerWins}<br>Computer's score: ${computerWins}`;
+        } else {
+            computerWins++;
+            return `You Lose!!! ${computer} beats ${player}.<br>
+            Your score: ${playerWins}<br>Computer's score: ${computerWins}`;
+        }
+    }
+
+    function displayResult(resultText) {
+        const results = document.querySelector("#Results");
+        results.innerHTML = resultText;
+    }
+    
+    function displayWinner(player, computer) {
+        const theWinner = document.querySelector("#Winner");
+
+        if (playerWins > computerWins) {
+            theWinner.innerHTML = "You Win!";
+        } else if (playerWins < computerWins) {
+            theWinner.innerHTML = "Computer Win!";
+        } else {
+            theWinner.innerHTML = "T's a Tie!";
+        }
+    }
+
+    function newGame() {
+        playerWins = 0;
+        computerWins = 0;
+        roundsPlayed = 0;
+
+        const results = document.querySelector("#Results");
+        results.innerHTML = "";
+
+        const theWinner = document.querySelector("#Winner");
+        theWinner.innerHTML = "";
+
+        console.log("New Game");
+    }
 }
+
+playGame ();
